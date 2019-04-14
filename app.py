f09ec0d88b12474547260621b557f33fb59b1cfb
martin2urba <martin.urban@rothamsted.ac.uk>
2019-04-14 14:48:45 +0200

    2019-04-14 14:48 commit

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 #on Windows in Cs50AndWeb directory do:
-# set  FLASK_APP=app.py
+# set FLASK_APP=app.py
+#changes
 #  set FLASK_DEBUG=1    ##allows auto reload
 # then start application with:
 #  $flask run
 #  $python -m flask run
 #  set FLASK_DEBUG=1    ##allows auto reload
 # find result on localhost:5000
-import datetime
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-    now=datetime.datetime.now()
-    new_year=1 #now.month == 1 and now.day()
-    return render_template("index.html", new_year=new_year)
+    return render_template("index.html")
+
+@app.route("/more")
+def more():
+    return render_template("more.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
