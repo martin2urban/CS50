2e93333b776c17890856f8796412b7550ac89377
martin2urba <martin.urban@rothamsted.ac.uk>
2019-04-15 08:50:37 +0200

    Form submission using flask and python

--- app.py
+++ app.py
@@ -7,15 +7,16 @@
 #  $python -m flask run
 #  set FLASK_DEBUG=1    ##allows auto reload
 # find result on localhost:5000
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/more")
-def more():
-    return render_template("more.html")
+@app.route("/hello", methods= ["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template("hello.html", name=name)
 
 
 if __name__ == "__main__":
